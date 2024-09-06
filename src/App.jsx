77d1e649b4319144a3ce5b5186fac5a9d6c1fa87
Lasma Poksane <laasmapoksane@gmail.com>
2024-09-07 00:40:55 +0200

    add CssBaseline to fix inconsistencies across browsers

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Landing from "./components/Landing";
 import Register from "./containers/Register";
 import Login from "./containers/Login";
 import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
