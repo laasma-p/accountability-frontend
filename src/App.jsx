92522dd6c30eb0040cb12e49a81bdfd1ce60ec5a
Lasma Poksane <laasmapoksane@gmail.com>
2024-09-07 00:39:05 +0200

    add theme to the app to use custom colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing";
 import Register from "./containers/Register";
 import Login from "./containers/Login";
+import { ThemeProvider } from "@emotion/react";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </ThemeProvider>
   );
 };
 
