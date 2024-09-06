cadcea46c9405973887dac6e0168f5ba7617bc8f
Lasma Poksane <laasmapoksane@gmail.com>
2024-09-06 21:21:42 +0200

    create initial Landing component

---
+++ src/components/Landing.jsx
@@ -0,0 +1,12 @@
+const Landing = () => {
+  return (
+    <div>
+      <h1>Welcome to Accountable</h1>
+      <p>Track your habits and keep yourself accountable with friends.</p>
+      <button>Log In</button>
+      <button>Register</button>
+    </div>
+  );
+};
+
+export default Landing;
