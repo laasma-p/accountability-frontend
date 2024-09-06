86cd3d0db663edd3c66ea2d4050568a27fa8eccb
Lasma Poksane <laasmapoksane@gmail.com>
2024-09-07 01:03:26 +0200

    add a switch to toggle from light to dark mode and vice versa

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,77 @@
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing";
 import Register from "./containers/Register";
 import Login from "./containers/Login";
-import { ThemeProvider } from "@emotion/react";
-import { CssBaseline } from "@mui/material";
-import { theme } from "./theme";
+import { lightTheme, darkTheme } from "./themes";
+import {
+  ThemeProvider,
+  CssBaseline,
+  IconButton,
+  Box,
+  Tooltip,
+} from "@mui/material";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
 const App = () => {
+  const [switchMode, setSwitchMode] = useState(() => {
+    return localStorage.getItem("theme") === "dark";
+  });
+
+  const toggleTheme = () => {
+    const newMode = !switchMode;
+    setSwitchMode(newMode);
+    localStorage.setItem("theme", newMode ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setSwitchMode(storedTheme === "dark");
+    }
+  }, []);
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={switchMode ? darkTheme : lightTheme}>
       <CssBaseline />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+          position: "relative",
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: 16,
+            right: 16,
+            display: "flex",
+            alignItems: "center",
+            zIndex: 1201,
+          }}
+        >
+          <Tooltip
+            title={switchMode ? "Switch to light mode" : "Switch to dark mode"}
+          >
+            <IconButton onClick={toggleTheme}>
+              {switchMode ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+          </Tooltip>
+        </Box>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Landing toggleTheme={toggleTheme} switchMode={switchMode} />
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Box>
     </ThemeProvider>
   );
 };
