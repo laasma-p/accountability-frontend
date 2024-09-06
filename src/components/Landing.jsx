19100c185c8f671bece65f3d03fe36c24ef30895
Lasma Poksane <laasmapoksane@gmail.com>
2024-09-06 21:29:02 +0200

    add links to access Login and Register components

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Landing = () => {
   return (
     <div>
       <h1>Welcome to Accountable</h1>
       <p>Track your habits and keep yourself accountable with friends.</p>
-      <button>Log In</button>
-      <button>Register</button>
+      <Link to="/login">Log In</Link>
+      <Link to="/register">Register</Link>
     </div>
   );
 };
