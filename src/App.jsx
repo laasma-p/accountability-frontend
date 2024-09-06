e661a58b8298ec6bfa8846e9c363a8ce554b4c9b
Lasma Poksane <laasmapoksane@gmail.com>
2024-09-06 21:30:47 +0200

    add routing and render Login and Register components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing";
+import Register from "./containers/Register";
+import Login from "./containers/Login";
 
 const App = () => {
-  return <Landing />;
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 };
 
 export default App;
