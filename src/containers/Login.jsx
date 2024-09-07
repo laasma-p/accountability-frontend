9cf69bc83089dd7c7f60db39d38be3f9b055bc3b
Lasma Poksane <laasmapoksane@gmail.com>
2024-09-07 15:54:31 +0200

    initially style Login component with MaterialUI

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,27 +1,71 @@
-import { Formik, Form, Field } from "formik";
+import { Formik, Form } from "formik";
+import { Box, TextField, Typography, Button } from "@mui/material";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <Formik initialValues={{ email: "", password: "" }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        padding: 2,
+      }}
+    >
+      <Typography
+        variant="h2"
+        sx={{
+          fontSize: { xs: "3rem", sm: "4rem" },
+          textAlign: "center",
+          marginBottom: 3,
+        }}
+      >
+        Login
+      </Typography>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+      >
         {() => {
           return (
-            <Form>
-              <div>
-                <label>E-mail</label>
-                <Field name="email" type="email" />
-              </div>
-              <div>
-                <label>Password</label>
-                <Field name="password" type="password" />
-              </div>
-              <button type="submit">Log In</button>
+            <Form style={{ width: "100%", maxWidth: "400px" }}>
+              <Box sx={{ marginBottom: 3 }}>
+                <TextField
+                  fullWidth
+                  name="email"
+                  label="E-mail"
+                  variant="outlined"
+                  sx={{ marginBottom: "24px" }}
+                />
+                <TextField
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="passowrd"
+                  variant="outlined"
+                  sx={{ marginBottom: "24px" }}
+                />
+                <Button
+                  fullWidth
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  sx={{
+                    padding: "10px 20px",
+                    fontSize: { xs: "0.875rem", sm: "1rem" },
+                  }}
+                >
+                  Log In
+                </Button>
+              </Box>
             </Form>
           );
         }}
       </Formik>
-    </div>
+    </Box>
   );
 };
 
