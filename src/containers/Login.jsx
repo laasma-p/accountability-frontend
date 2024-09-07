710f855a422f2f00e3d41ddd34866cd8f8cbe77d
Lasma Poksane <laasmapoksane@gmail.com>
2024-09-08 00:45:36 +0200

    add initial logging in and validation functionality

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,7 +1,34 @@
 import { Formik, Form } from "formik";
 import { Box, TextField, Typography, Button } from "@mui/material";
+import * as Yup from "yup";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid e-mail format.")
+    .required("E-mail is required.")
+    .matches(/^\S*$/, "E-mail must not contain spaces."),
+  password: Yup.string()
+    .required("Password is required.")
+    .matches(/^\S*$/, "Password must not contain spaces."),
+});
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const loginHandler = (values) => {
+    axios
+      .post("http://localhost:3000/api/login", values)
+      .then((response) => {
+        localStorage.setItem("token", response.data.token);
+        navigate("/dashboard");
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+
   return (
     <Box
       sx={{
@@ -28,8 +55,10 @@ const Login = () => {
           email: "",
           password: "",
         }}
+        validationSchema={LoginSchema}
+        onSubmit={loginHandler}
       >
-        {() => {
+        {({ handleChange, values }) => {
           return (
             <Form style={{ width: "100%", maxWidth: "400px" }}>
               <Box sx={{ marginBottom: 3 }}>
@@ -38,14 +67,18 @@ const Login = () => {
                   name="email"
                   label="E-mail"
                   variant="outlined"
+                  onChange={handleChange}
+                  value={values.email}
                   sx={{ marginBottom: "24px" }}
                 />
                 <TextField
                   fullWidth
                   name="password"
                   label="Password"
-                  type="passowrd"
+                  type="password"
                   variant="outlined"
+                  onChange={handleChange}
+                  value={values.password}
                   sx={{ marginBottom: "24px" }}
                 />
                 <Button
