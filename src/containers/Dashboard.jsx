d962ae5e34413490dfe35b955e860459e2537d69
Lasma Poksane <laasmapoksane@gmail.com>
2024-09-08 19:25:58 +0200

    add an AppBar with the theme switch

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -1,5 +1,44 @@
-const Dashboard = () => {
-  return <h1>Dashboard will be here.</h1>;
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Tooltip,
+} from "@mui/material";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+
+const Dashboard = ({ toggleTheme, switchMode }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" noWrap sx={{ flexGrow: 1 }}>
+            Accountable
+          </Typography>
+          <Tooltip
+            title={switchMode ? "Switch to light mode" : "Switch to dark mode"}
+          >
+            <IconButton color="inherit" onClick={toggleTheme}>
+              {switchMode ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+          </Tooltip>
+        </Toolbar>
+      </AppBar>
+      <Box>
+        <Typography variant="body1">Dashboard will be here.</Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
