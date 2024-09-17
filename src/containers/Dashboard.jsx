8958e7b0fe201c61b1942f3984cb1f47773e8b67
Lasma Poksane <laasmapoksane@gmail.com>
2024-09-17 13:48:59 +0200

    render today's tracked habits

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -1,7 +1,63 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, List, ListItem } from "@mui/material";
 import Calendar from "./Calendar";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Dashboard = () => {
+  const [trackedHabits, setTrackedHabits] = useState([]);
+  const [currentDate, setCurrentDate] = useState(null);
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    const fetchDate = async () => {
+      const today = new Date().toISOString().split("T")[0];
+      setCurrentDate(new Date(today));
+
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/habits/date/${today}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+
+        setTrackedHabits(Array.isArray(response.data) ? response.data : []);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchDate();
+  }, [token]);
+
+  const fetchTrackedHabits = async (date) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/habits/date/${date}`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      return Array.isArray(response.data) ? response.data : [];
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handleDateSelect = async (date) => {
+    try {
+      const habits = await fetchTrackedHabits(date);
+      if (Array.isArray(habits)) {
+        setTrackedHabits(habits);
+      } else {
+        console.error(error.message);
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -23,7 +79,28 @@ const Dashboard = () => {
         Dashboard
       </Typography>
       <Box>
-        <Calendar />
+        <Calendar onDateSelect={handleDateSelect} />
+        <Box sx={{ marginTop: 2 }}>
+          <Typography variant="h6">Today's habits:</Typography>
+          {Array.isArray(trackedHabits) && trackedHabits.length > 0 ? (
+            <List>
+              {trackedHabits.map((habit) => {
+                return (
+                  <ListItem
+                    sx={{ display: "flex", justifyContent: "center" }}
+                    key={habit.id}
+                  >
+                    {habit.name}
+                  </ListItem>
+                );
+              })}
+            </List>
+          ) : (
+            <Typography variant="body1" sx={{ marginTop: 2 }}>
+              No habits tracked.
+            </Typography>
+          )}
+        </Box>
       </Box>
     </Box>
   );
